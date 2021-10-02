cb79ec0479c0ac47e79c70b6ef14247106e07161
Antos12 <antosofiag@gmail.com>
2021-10-01 22:22:20 -0300

    entrega proyecto final

--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
 function compararDatosJson(usuario,contrasenia){
-  console.log("aca llega");
+  
   fetch('../js/clientes.json')
    .then(function(respuesta){
        return respuesta.json();
@@ -10,17 +10,30 @@ function compararDatosJson(usuario,contrasenia){
         
        let datosUsuario1JSON = clientes[0].usuario;
        let datosContrasenia1JSON = clientes[0].contra;
-       console.log(datosUsuario1JSON);
-       console.log(datosContrasenia1JSON);
-       if (usuario==datosUsuario1JSON && contrasenia== datosContrasenia1JSON){
-           alert("Has ingresado")
-       } else {
-           alert("No es un cliente registrado porfavor registrese")
-       }
+       let datosUsuario2JSON = clientes[1].usuario;
+       let datosContrasenia2JSON = clientes[1].contra;
+       let datosUsuario3JSON = clientes[2].usuario;
+       let datosContrasenia3JSON = clientes[2].contra;
+       let datosUsuario4JSON = clientes[3].usuario;
+       let datosContrasenia4JSON = clientes[3].contra;
+       if(usuario==datosUsuario1JSON && contrasenia== datosContrasenia1JSON){
+        alert("Has ingresado")
+       }else{ if (usuario==datosUsuario2JSON && contrasenia== datosContrasenia2JSON){
+        alert("Has ingresado")
+       }else{ if (usuario==datosUsuario3JSON && contrasenia== datosContrasenia3JSON){
+        alert("Has ingresado")
+       }else { if (usuario==datosUsuario4JSON && contrasenia== datosContrasenia4JSON){
+        alert("Has ingresado")
+
+       }else { alert("No es un cliente registrado porfavor registrese")
+       }}}}
+   })    
+} 
+
       
-   })
+   
      
-}
+
 
 
 /* Traer datos de un formulario de Log In */
